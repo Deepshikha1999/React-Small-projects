83265e5a29631f832f237a7808467daebccf2f42
Deepshikha Sahoo <deepshikha01081999@gmail.com>
2026-01-16 22:49:15 +0530

    prod deployement

--- pixie_webpage/vite.config.js
+++ pixie_webpage/vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/", // To run with npm run build in prod
+  build: {
+    outDir: "dist",
+  },
   plugins: [
     react({
       babel: {
